760c768fb45c8c01d316e08dcdbcd706faec877c
rinkesh <rinkeshjain15@gmail.com>
2017-02-27 02:01:23 +0530

    Udpate to Origin

---
+++ app/src/main/java/com/rinkesh/btconnector/CallReceiver.java
@@ -0,0 +1,15 @@
+package com.rinkesh.btconnector;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by rinkesh on 27/02/17.
+ */
+public class CallReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
